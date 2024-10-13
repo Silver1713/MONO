70b6a092137429756f7baf1eb6d50a41e3266b42
Yeo Jia Hao <jiahao.yeo@digipen.edu>
2024-10-13 21:46:32 +0800

    Full

--- Install.bat
+++ Install.bat
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f6f959751988b19d993ee032d3a755bcb5695e7355f586732f7d2c0342c130f2
-size 183
+@echo off
+git clone https://github.com/Silver1713/MONO MONO_INSTALL
+
+pause
